16afa1d7f08a43cc9b7e62f76cec39f1f02d2a26
mkleung-pcms <116505065+mkleung-pcms@users.noreply.github.com>
2022-10-24 17:47:30 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
     x = 0
-})
-basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        let y = 0
-        led.plot(x, y)
+    while (x <= 4) {
+        led.plot(x, 0)
         basic.pause(500)
-        led.unplot(x, y)
+        led.unplot(x, 0)
         x += 1
     }
+})
+input.onButtonPressed(Button.B, function () {
+    y = 0
+    while (y <= 4) {
+        led.plot(0, y)
+        basic.pause(500)
+        led.unplot(0, y)
+        y += 1
+    }
+})
+basic.forever(function () {
+	
 })
